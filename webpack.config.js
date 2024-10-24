11a2e246c5711f52eba301fe269cada26e92652e
James Morrissey <jammim19@gmail.com>
2024-10-24 12:57:43 +0100

    Updated the webpack config for the recipes page

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require("path");
 
-module.exports = {
-  mode: "production",
-  entry: "./recipe-page-main/src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "./recipe-page-main/dist"),
+module.exports = [
+  {
+    mode: "production",
+    entry: {
+      recipe_page: "./recipe-page-main/src/index.js",
+    },
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "./recipe-page-main/dist"),
+    },
   },
-};
+];
