111d817ef9c287187db13a95bb1775e363836cf5
James Morrissey <jammim19@gmail.com>
2024-11-15 18:45:34 +0000

    Added a webpack configuration for the product preview card component

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,45 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = [
   {
-    mode: "production",
+    mode: "development",
     entry: {
-      recipe_page: "./recipe-page-main/src/index.js",
+      product_preview_card_component:
+        "./product-preview-card-component-main/src/index.js",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "/product-preview-card-component-main/src/index.html",
+      }),
+    ],
+    devtool: "inline-source-map",
+    devServer: {
+      static: "./product-preview-card-component-main",
     },
     output: {
-      filename: "main.js",
-      path: path.resolve(__dirname, "./recipe-page-main/dist"),
+      filename: "[name].bundle.js",
+      path: path.resolve(
+        __dirname,
+        "./product-preview-card-component-main/dist"
+      ),
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: "asset/resource",
+        },
+      ],
     },
   },
 ];
