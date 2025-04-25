608b9916446dee0fbd9c2a0641f7d9e9ac5a04cf
James Morrissey <jammim19@gmail.com>
2025-04-25 22:05:19 +0100

    Added some final fixes for detecting accessibility issues

--- product-preview-card-component-main/src/index.js
+++ product-preview-card-component-main/src/index.js
@@ -3,3 +3,15 @@ import "./style.css";
 window.addEventListener("load", function () {
   console.log("All assets are loaded");
 });
+
+axe
+  .run()
+  .then((results) => {
+    if (results.violations.length) {
+      console.log(results.violations);
+      throw new Error("Accessibility issues found");
+    }
+  })
+  .catch((err) => {
+    console.error("Something bad happened:", err.message);
+  });
